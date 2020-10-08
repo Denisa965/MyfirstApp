5663ec40e6bd20ea7502362baa9dbf3af0f5add0
ALEXANDRA-DENISA BOCA <boca.alexandra165205@student.ubbcluj.ro>
2020-10-08 17:01:09 +0300

    p2

--- MyfirstApp/Program.cs
+++ MyfirstApp/Program.cs
@@ -6,7 +6,7 @@ namespace MyfirstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World !");
         }
     }
 }
